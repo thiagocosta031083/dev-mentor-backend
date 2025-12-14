d177142afc26378a261b164310f4952ded8c7ed6
Thiago Costa <thiagocosta031083@gmail.com>
2025-12-14 03:38:59 -0300

    feat: criar estrutura inicial do projeto Spring Boot
    Closes #1

---
+++ src/main/java/com/thiagocosta/devmentor/backend/DevMentorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thiagocosta.devmentor.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevMentorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevMentorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagocosta/devmentor/backend/DevMentorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagocosta.devmentor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevMentorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
